aeec01ab52238e6698fe44ba97b4ed10be67a7ff
CozyCrypto <fassdavid722@gmail.com>
2026-07-10 23:23:49 +0100

    scaffold: create src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from './types';
+export { PluginManager } from './PluginManager/manager';
+export { groqPlugin } from './AIProviders/groq-plugin';
+export { githubPlugin } from './GitHub/github-plugin';
+export { base44Plugin } from './Base44/base44-plugin';
